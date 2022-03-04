d11c573f99b4644711d5754e5d611c37f196b8aa
Your Name <vonam235689741.work@gmail.com>
2022-03-04 15:36:42 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 require('./bootstrap');
-require('font-awesome/css/font-awesome.min.css')
-=======
-require("./bootstrap");
+// require('font-awesome/css/font-awesome.min.css');
+
+
 import Swiper from "swiper";
 import "swiper/css";
 
@@ -14,4 +14,3 @@ const swiper = new Swiper(".swiper", {
         delay: 2000,
     },
 });
->>>>>>> 95740977a8f65e4b1b467652241f07c961a84ab6
