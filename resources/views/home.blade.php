d9a54c1ffeaf51d9d904a4a65d7490631abf936b
Your Name <vonam235689741.work@gmail.com>
2022-03-04 14:41:48 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,21 +11,26 @@
                 
             </div>
         </section>
+        {{-- Middle section start --}}
         <section class="mid">
             <div class="container-md">
                 <div class="logo">
-                    <img src="images/Logo-GEARVN_pc-300x70-1-1.png" alt="">
+                    <img src="{{asset('images/Logo-GEARVN_pc-300x70-1-1.png')}}" alt="">
                 </div>
                 <div class="search">
                     <form action="#">
                         <input type="text" placeholder="Bạn tìm gì..">
                         <button>
-                            <i class="ph-magnifying-glass"></i>
+                            <i class="fas fa-search"></i>
+                            <span>Tìm kiếm</span>
                         </button>
                     </form>
                 </div>
+                <div class="user"></div>
+                <div class="cart"></div>
             </div>
         </section>
+        {{-- Middle Section End --}}
         <section class="bottom">
 
         </section>
