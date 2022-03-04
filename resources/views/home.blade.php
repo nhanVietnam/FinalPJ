1cc918a1dba95c71c474809eafb7123cef573bbc
Nhan <lethanhnhan14698@gmai.com>
2022-03-04 13:49:04 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,19 @@
 @section('title','GEARVN - Trang chủ')
 
 @section('content')
-    
+    <header>
+        <section class="top">
+            <div class="slide">
+            </div>
+            <div class="list">
+                
+            </div>
+        </section>
+        <section class="mid">
+
+        </section>
+        <section class="bottom">
+
+        </section>
+    </header>
 @endsection
